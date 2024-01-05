a414436b1363866609a350e9835733f41b2b890f
Jae Sin Cho <1005jsc@naver.com>
2024-01-05 11:09:11 +0900

    certbot authenticate를 얻기위해 시도하기

---
+++ controller/certbot.js
@@ -0,0 +1,4 @@
+// 목표리스트 get(완료)
+export const getGoalList = async (req, res, next) => {
+  res.status(200).json('hihihi');
+};
---
+++ router/certbot.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import 'express-async-errors';
+
+import * as CertbotController from '../controller/certbot.js';
+
+const router = express.Router();
+
+// 목표 get
+router.get('/.well-known/acme-challenge', CertbotController.getGoalList);
+
+export default router;
